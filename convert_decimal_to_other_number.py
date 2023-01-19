35dc829920eff9b2b5c927079a949841ecbdde74
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2023-01-19 21:48:16 +0100

    Add files via upload

---
+++ convert_decimal_to_other_number.py
@@ -0,0 +1,11 @@
+# A python program to convert decimal number into other number
+
+dec = int(input("Enter a Decimal number: "))
+# Decimal to Binary
+print(bin(dec), "In Binary.")
+
+# Decimal to Octal
+print(oct(dec), "In Octal.")
+
+# Decimal to Hexadecimal
+print(hex(dec), "In Hexadecimal.")
